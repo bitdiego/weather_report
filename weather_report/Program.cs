edbd1abb48157eec6dc61e0553c5962f60eb6dec
diego polidori <d.poldo@libero.it>
2022-09-23 17:16:16 +0200

    Added interactive input parameters for application User must enter a valid API key of weatherapi.com

--- weather_report/Program.cs
+++ weather_report/Program.cs
@@ -7,7 +7,7 @@ using weather_report.Models;
 using weather_report.Services;
 var watch = System.Diagnostics.Stopwatch.StartNew();
 
-/*
+
 if (args.Count() != 3)
 {
     Console.WriteLine("Error: wrong numbers of input parameters");
@@ -19,7 +19,7 @@ if(String.Compare(args[1], "--key", true) != 0)
     return;
 }
 Globals.WEATHER_API_KEY = args[2];
-*/
+
 
 CitiesApiConsumer consumer = new CitiesApiConsumer(new CityDataParser<City>());
 try
@@ -32,14 +32,6 @@ try
         WeatherApiConsumer weather=new WeatherApiConsumer(new DynamicDataParser<WeatherReport>());
         await weather.GetForecastByCityCoordinates(city.Latitude, city.Longitude);
         logger.LogForecast(city.Name, weather.EnumForecast());
-        /*string uri = String.Format(Globals.WEATHER_FORECAST_API_URL, Globals.WEATHER_API_KEY, city.Latitude.ToString() + "," + city.Longitude.ToString());
-        string weatherResponse = await RequestHandler.SendGetRequestAsync(uri);
-        IStringDataParser<WeatherReport> wrParser = new DynamicDataParser<WeatherReport>();
-        var weatherReportList = wrParser.GetDataListFromString(weatherResponse);
-        foreach (var forecast in weatherReportList)
-        {
-            Console.WriteLine("Processed city {0} | {1} - {2}", city.Name, weatherReportList[0].Text, weatherReportList[1].Text);
-        }*/
     }
 }
 catch (Exception ex)
